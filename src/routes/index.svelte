8d6f1fa9cf8f8e7eefded0d805058576d819bea4
Ben Sooter <sooterbw@gmail.com>
2022-06-19 00:47:18 -0500

    Configure Tailwind and setup initial game board

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from 'svelte';
+
+    let position = 350;
+    let keys = [];
+    let wordList = ['Strong', 'Noisy', 'Imperviousness']
+
+    function handleKey(e) {
+        console.log(typeof(e))
+        keys[e.keyCode] = true;
+    }
+
+    function handleUp(e) {
+        keys[e.keyCode] = false;
+    }
+
+    function checkWord() {
+        
+    }
+
+    onMount(() => {
+        function update() {
+            requestAnimationFrame(update);
+            if (keys[37]) {
+                position--
+            }
+            if (keys[39]) {
+                position++
+            }
+
+            if(position <= 0) {
+                position = 0
+            }
+            if (position >= 700) {
+                position = 700
+            }
+        }
+        update();
+    })
+
+
+</script>
+
+<svelte:window on:keydown|preventDefault="{handleKey}" on:keyup|preventDefault="{handleUp}" />
+
+<div class="w-[800px] h-[800px] bg-slate-200 flex flex-col justify-between">
+    <div class="w-[100px] h-[100px] bg-red-500"></div>
+    <div style="transform: translateX({position}px);" class="w-[100px] h-[100px] bg-blue-500"></div>
+</div>
